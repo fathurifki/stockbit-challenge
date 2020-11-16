56314f97cbe2feacb9f8227b8fbbab2894b226c0
fathurifki <rifkifathu@gmail.com>
2020-11-16 23:22:08 +0700

    configure store & tailwind

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import thunkMiddleware from 'redux-thunk'
+import rootReducer from './reducers'
+
 import logo from './logo.svg';
 import './App.css';
 
+import loggerMiddleware from './utils/logger';
+import monitorReducerEnhancer from './utils/monitorReducer';
+import history from './utils/browserHistory'
+
 function App() {
+
+  const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+  const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
+  const store = createStore(rootReducer, undefined, composedEnhancers)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <Provider store={store}>
+      <Router history={history}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
         </a>
-      </header>
-    </div>
+          </header>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
